1fd22989132076f62f4833605841573bf4380eeb
crepe-japheth <mutabarukajaphett@gmail.com>
2025-05-21 00:31:34 +0200

    added leading slash on static_url

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = [
 #     BASE_DIR/'static',
 # ]
@@ -118,7 +118,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_ROOT = BASE_DIR/'media'
 
-MEDIA_URL = 'media/'
+MEDIA_URL = '/media/'
 
 
 # Default primary key field type
